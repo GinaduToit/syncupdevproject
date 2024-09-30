a6e10886a80bc5197d1e383bfc4b712c6d78a1f1
Gina du Toit <ginadutoit02@gmail.com>
2024-09-30 10:01:46 +0200

    set up app

---
+++ README.md
@@ -0,0 +1,3 @@
+# syncupdevproject
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/syncupdevproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.syncupdevproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
